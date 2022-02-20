546bfc502ba8756f650ac9455ea629098ff7c60e
JoãoVitorAndrade <andradejaum@gmail.com>
2022-02-20 12:43:58 -0300

    CursoFinalizado

--- App.js
+++ App.js
@@ -1,17 +1,31 @@
 import React, { Component } from "react";
 import "./App.css";
 import FormularioCadastro from "./components/FormularioCadastro/FormularioCadastro";
-import 'fontsource-roboto';
+import "fontsource-roboto";
 
-import {Container, Typography } from "@material-ui/core"
+import { Container, Typography } from "@material-ui/core";
 class App extends Component {
   render() {
     return (
       <Container component="article" maxWidth="sm">
-        <Typography variant="h3" component="h1" align="center" >Formulário de cadastro</Typography>
-        <FormularioCadastro />
+        <Typography variant="h3" component="h1" align="center">
+          Formulário de cadastro
+        </Typography>
+        <FormularioCadastro aoEnviar={aoEnviarForm} validarCPF={validarCPF} />
       </Container>
     );
+  }
+}
+
+function aoEnviarForm(dados) {
+  console.log(dados);
+}
+
+function validarCPF(cpf) {
+  if (cpf.length !== 11) {
+    return { valido: false, texto: "CPF deve conter 11 dígitos!" };
+  }else {
+    return { valido: true, texto: "" };
   }
 }
 
